4668048c2878d4496606dedc087794e62e7cfb17
Norman Pendzich <norman.pendzich@outlook.de>
2022-08-04 00:22:16 +0200

    AES 192 cbc verschlüsselung fertig

---
+++ cipher.js
@@ -0,0 +1,32 @@
+"use srict";
+const readline = require("readline-sync");
+const crypto = require("crypto");
+
+const algorithm = "aes-192-cbc";
+let password = readline.question("Geben Sie das Passwort ein:");
+let text = readline.question("Geben Sie den zu verschlüsselnden Text ein: ");
+
+
+// Zunächst wird der Schlüssel generiert, da er vom Algorithmus abhängig ist.
+// In diesem Fall für aes192 ist der Schlüssel 24 Bytes (192 Bits).
+crypto.scrypt(text, password, 24, (err, key) => {
+  if (err) throw err;
+  // Danach erzeugen wir einen Zufallsvektor (Initialisierungsvektor)
+  crypto.randomFill(new Uint8Array(16), (err, iv) => {
+    if (err) throw err;
+
+    // Cipher mit Schlüssel und iv erstellen
+    const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+    let encrypted = "";
+    cipher.setEncoding("hex");
+
+    cipher.on("data", (chunk) => (encrypted += chunk));
+    cipher.on("end", () =>
+      console.log(text + "wird zu " + encrypted + " verschlüsselt")
+    ); // gibt verschlüsselte Daten aus
+
+    cipher.write("some clear text data");
+    cipher.end();
+  });
+});
