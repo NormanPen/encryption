f66a2da4aad4c3f60e9970d23eaee3683497553d
Norman Pendzich <norman.pendzich@outlook.de>
2022-08-04 14:10:52 +0200

    cipher function works

--- cipher.js
+++ cipher.js
@@ -1,31 +1,47 @@
 "use srict";
 const readline = require("readline-sync");
 const crypto = require("crypto");
+const fs = require("fs");
 
 const algorithm = "aes-192-cbc";
 let password = readline.question("Geben Sie das Passwort ein:");
 let text = readline.question("Geben Sie den zu verschlüsselnden Text ein: ");
 
-// Zunächst wird der Schlüssel generiert, da er vom Algorithmus abhängig ist.
-// In diesem Fall für aes192 ist der Schlüssel 24 Bytes (192 Bits).
-crypto.scrypt(password, "salt", 24, (err, key) => {
-  if (err) throw err;
-  // Danach erzeugen wir einen Zufallsvektor (Initialisierungsvektor)
-  crypto.randomFill(new Uint8Array(16), (err, iv) => {
+const start = () => {
+  let again = readline.question("Speichern? (y/n)");
+
+  if (again == "y") {
+    startCrypto();
+  } else {
+    process.exit(1);
+  }
+};
+
+const startCrypto = () => {
+  crypto.scrypt(password, "salt", 24, (err, key) => {
     if (err) throw err;
 
-    // Cipher mit Schlüssel und iv erstellen
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
+    crypto.randomFill(new Uint8Array(16), (err, iv) => {
+      if (err) throw err;
 
-    let encrypted = "";
-    cipher.setEncoding("hex");
+      const cipher = crypto.createCipheriv(algorithm, key, iv);
 
-    cipher.on("data", (chunk) => (encrypted += chunk));
-    cipher.on("end", () =>
-      console.log(text + "wird zu " + encrypted + " verschlüsselt")
-    ); // gibt verschlüsselte Daten aus
+      let encrypted = "";
+      const file = fs.createWriteStream("tresor.txt");
+      cipher.setEncoding("hex");
 
-    cipher.write(text);
-    cipher.end();
+      cipher.on("data", (chunk) => (encrypted += chunk));
+      cipher.on(
+        "end",
+        () => file.write(encrypted),
+        console.log("Verschlüsselung in tresor.txt gespeicher")
+      );
+
+      cipher.write(text);
+
+      cipher.end();
+    });
   });
-});
+};
+
+start();
